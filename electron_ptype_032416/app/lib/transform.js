d75a35ef59198c1956fa60950b1c27e63c9e67c9
Aaron Freedman <aaron@secretcrush.net>
2016-03-28 11:12:13 -0400

    added distance methods to Matrix for Transform

--- electron_ptype_032416/app/lib/transform.js
+++ electron_ptype_032416/app/lib/transform.js
@@ -1,8 +1,20 @@
 'use strict';
 
-let linearAlgebra = require('linear-algebra')(),
+const linearAlgebra = require('linear-algebra')(),
 	Vector = linearAlgebra.Vector,
 	Matrix = linearAlgebra.Matrix;
+
+Matrix.sqrDistance = function (fromVector, toVector) {
+	let displacement = fromVector.minus(toVector);
+	let sqrDist = displacement.map(function(v) {
+		return v * v;
+	});
+	return sqrDist.getSum();
+};
+
+Matrix.distance = function(fromVector, toVector) {
+	return Math.sqrt(Matrix.sqrDistance(fromVector, toVector));
+};
 
 export class Transform {
 	constructor(position) {
@@ -11,10 +23,11 @@ export class Transform {
 
 	translate(translation) {
 		this.position = this.position.plus(translation);
+		return this.position.toArray();
 	}
 
 	translate(x, y, z) {
 		this.position = this.position.plus(new Matrix([x, y, z]));
-		return this.position.toArray()
+		return this.position.toArray();
 	}
 }
