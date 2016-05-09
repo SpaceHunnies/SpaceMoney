8ca2e27c15105543bb8f7acb4eb7659002d091bf
Aaron Freedman <aaron@secretcrush.net>
2016-05-09 13:48:12 -0400

    star system names

--- app/lib/universe/star-system.js
+++ app/lib/universe/star-system.js
@@ -5,6 +5,21 @@ export class StarSystem extends GameObject {
 	constructor(args, transform) {
 		super(args, transform);
 		this.starport = args.starport;
+		this.name = this.generateName();
+	}
+
+	generateName() {
+	    var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	    var numbers = "0123456789";
+
+	    let a = "";
+	    let b = "";
+	    for( var i=0; i < 3; i++ ) {
+	        a += letters.charAt(Math.floor(Math.random() * letters.length));
+	        b += numbers.charAt(Math.floor(Math.random() * numbers.length));
+	    }
+
+	    return a + '-' + b;
 	}
 
 	print() {
